14a144f6f234635d8a8b5de1c95189647bb929d4
Ilya Maier <ilya.maier@revelsystems.com>
2020-11-08 11:27:25 +0300

    Skeleton + Pods + Swiftlint

---
+++ NewsApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NewsApp
+//
+//  Created by Ilya Maier on 08.11.2020.
+//  Copyright © 2020 Orion Innovation. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NewsApp
+//
+//  Created by Ilya Maier on 08.11.2020.
+//  Copyright © 2020 Orion Innovation. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
